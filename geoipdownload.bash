ed3913870f8527c23bcf6bd9c944786185e00e7d
Dmitriy Kepov <dk.dkepov@gmail.com>
2026-05-29 19:15:39 +0300

    GeoIP Download Script with Infos

--- geoipdownload.bash
+++ geoipdownload.bash
@@ -14,6 +14,8 @@ if [ ! -f "$GeoIPConfName" ]; then
   exit 1
 fi
 
+echo "Info: The configuration file '$GeoIPConfName' was found."
+
 # Getting the License Key
 
 LicenseKeyVar=$(cat $GeoIPConfName | grep -oP '^LicenseKey\s+\K\S+')
@@ -25,6 +27,8 @@ if [ -z "$LicenseKey" ]; then
   echo "Error: The 'LicenseKey' parameter is empty or missing in '$GeoIPConfName'!" >&2
   exit 1
 fi
+
+echo "Info: The license key has been successfully read."
 
 # Getting the EditionIDs GeoIP Bases
 # Making List of GeoIP Bases
@@ -46,10 +50,14 @@ fi
 declare -a EditionIDs
 read -ra EditionIDs <<< $EditionIDsVar;
 
+echo "Info: Download databases ${#EditionIDs[@]} found: (${EditionIDs[*]})"
+
 # Permanent link for downloading files
 
 DownloadLink="https://download.maxmind.com/app/geoip_download?edition_id=EDITION_ID&license_key=LICENSE_KEY&suffix=tar.gz"
 
+
+echo "Info: Starting the database download cycle..."
 
 # Walking through the array
 # And downloading database in current directory
@@ -57,7 +65,7 @@ DownloadLink="https://download.maxmind.com/app/geoip_download?edition_id=EDITION
 for EditionID in "${EditionIDs[@]}"
 do
 
-  echo $EditionID
+  echo "Info: Current base is $EditionID"
 
   EditionIDArchive=$EditionID.mmdb.gz
   EditionIDBase=$EditionID.mmdb
@@ -72,6 +80,8 @@ do
 
   # Downloading Bases
 
+  echo "Info: Download the archive from the MaxMind server..."
+
   curl $EditionDownloadLink --output $EditionIDArchive
 
   # Checking if the file has been downloaded, or is there an error from the api?
@@ -82,23 +92,34 @@ do
     cat "$EditionIDArchive" >&2
     echo "" >&2
     # Delete temporary downloading File
+    echo "Info: Temporary files deleted ..."
     rm -f "$EditionIDArchive"
     exit 1
   fi
 
+  echo "Info: The archive has been downloaded successfully."
+
   # Getting the path to the Destination file
+
+  echo "Info: Search for the path of the database file inside the archive..."
 
   EditionIDTargetPath=$(tar -tf $EditionIDArchive | grep $EditionIDBase)
 
   # Extracting the final file from the archive
 
+  echo "Info: Unpacking the database file..."
+
   tar -zxf $EditionIDArchive $EditionIDTargetPath
 
   # Moving the destination file to the current directory
 
+  echo "Info: Moving the file to the current directory..."
+
   mv $EditionIDTargetPath $EditionIDBase
 
   # Delete temporary Extractiong directory
+
+  echo "Info: Clearing temporary files and folders..."
 
   rm -rf $(dirname $EditionIDTargetPath)
 
@@ -106,4 +127,8 @@ do
 
   rm $EditionIDArchive
 
+  echo "Info: The $EditionID database has been updated."
+
 done
+
+echo "Info: All GeoIP databases have been successfully updated!"
