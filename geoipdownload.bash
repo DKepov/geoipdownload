c8c469c1d2354ee6603fa151f2e80be38d82996d
Dmitriy Kepov <dk.dkepov@gmail.com>
2026-05-29 20:10:35 +0300

    GeoIP Download Script TODO

--- geoipdownload.bash
+++ geoipdownload.bash
@@ -4,7 +4,7 @@
 # https://dev.maxmind.com/geoip/updating-databases/
 # https://github.com/maxmind/geoipupdate
 
-
+# TODO Need use real maxmind database directory
 # Will be working in current directory
 TargetDir=$(pwd)
 
