a8ed0dbb63ca426fae99d9430046e849f7f82bfb
Dmitriy Kepov <dk.dkepov@gmail.com>
2026-06-14 18:58:44 +0300

    For Tests

--- test/test.bash
+++ test/test.bash
@@ -48,17 +48,17 @@ run_or_next() {
   "${TARGET_COMMAND[@]}" 2>/dev/null || "${next_cmd[@]}"
 }
 
-mkdir -p "${CHECK_DIR}" || err 'Can not create the Test directory'
+run_or_next ::: mkdir -p "${CHECK_DIR}" :: err 'Can not create the Test directory'
 
 cp './../GeoIP.conf' "${CHECK_DIR}/GeoIP.conf"
 cp './../geoipdownload' "${CHECK_DIR}/geoipdownload"
 
 sed -i "s|DOWNLOAD_URL_TEMPLATE='.*'|DOWNLOAD_URL_TEMPLATE='file://${URL}/EDITION_ID.mmdb.tar.gz'|g" "${CHECK_DIR}/geoipdownload"
 
-cd "${CHECK_DIR}" || err 'Can not open the Test directory'
+run_or_next ::: cd "${CHECK_DIR}" :: err 'Can not open the Test directory'
 
 ./geoipdownload -f 'GeoIP.conf' -d '.' -v
 
-cd "${CURRENT_DIR}" || err 'Can not leave the Test directory '
+run_or_next ::: cd "${CURRENT_DIR}" :: err 'Can not leave the Test directory '
 
-rm -rf "${CHECK_DIR}" || err 'Can not remove the Test directory'
+run_or_next ::: rm -rf "${CHECK_DIR}" :: err 'Can not remove the Test directory'
