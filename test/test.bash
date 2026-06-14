42ef76d72f6055f491fc41c8c353d5ce505b7721
Dmitriy Kepov <dk.dkepov@gmail.com>
2026-06-14 17:40:55 +0300

    For Tests

--- test/test.bash
+++ test/test.bash
@@ -7,17 +7,22 @@ readonly CHECK_DIR="${CURRENT_DIR}/check"
 
 readonly URL="${CURRENT_DIR}/../test-data"
 
-mkdir -p "${CHECK_DIR}" || exit 1
+mkdir -p "${CHECK_DIR}" || err 'Can not create the Test directory'
 
 cp './../GeoIP.conf' "${CHECK_DIR}/GeoIP.conf"
 cp './../geoipdownload' "${CHECK_DIR}/geoipdownload"
 
 sed -i "s|DOWNLOAD_URL_TEMPLATE='.*'|DOWNLOAD_URL_TEMPLATE='file://${URL}/EDITION_ID.mmdb.tar.gz'|g" "${CHECK_DIR}/geoipdownload"
 
-cd "${CHECK_DIR}" || exit 1
+cd "${CHECK_DIR}" || err 'Can not open the Test directory'
 
 ./geoipdownload -f 'GeoIP.conf' -d '.' -v
 
-cd "${CURRENT_DIR}" || exit 1
+cd "${CURRENT_DIR}" || err 'Can not leave the Test directory '
 
-rm -rf "${CHECK_DIR}" || exit 1
+rm -rf "${CHECK_DIR}" || err 'Can not remove the Test directory'
+
+err() {
+    echo "$1" >&2
+    exit 1
+}
